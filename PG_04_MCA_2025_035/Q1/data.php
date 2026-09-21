0147a1a426472fa8aaa89fe0b6a41921ad300777
shreya5474 <shreya.mandal.5474@gmail.com>
2026-09-21 16:52:15 +0530

    added

---
+++ PG_04_MCA_2025_035/Q1/data.php
@@ -0,0 +1,10 @@
+<?php
+function calculateBill($quantity, $price)
+{
+    return $quantity * $price;
+}
+$quantity = $_POST["quantity"];
+$price = $_POST["price"];
+$total = calculateBill($quantity, $price);
+echo "Total Bill = ". $total;
+?>
