27d4f30662b20a5bf49fb4a7330ef0bc68527a03
ognishgit <iamognish@gmail.com>
2026-09-21 17:33:08 +0530

    Add files via upload

---
+++ PG04MCA2025037/question1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calculate Bill</title>
+</head>
+<body>
+
+<h1>Calculate Bill</h1>
+
+<form method="post" action="backend1.php">
+    Quantity:
+    <input type="number" name="quantity" required>
+    <br><br>
+
+    Price:
+    <input type="number" name="price" required>
+    <br><br>
+
+    <input type="submit" name="calculate" value="Calculate Bill">
+</form>
+
+</body>
+</html>
